cc1e2afc220d33b49a201412ab65e7b7ddcdb447
Nam-Luc Tran <namluc.tran@euranova.eu>
2015-03-12 16:19:36 +0100

    first commit continued

---
+++ flink-ssp-parameter-server/src/main/java/org/flink/ssp/parameter/server/App.java
@@ -0,0 +1,11 @@
+package org.flink.ssp.parameter.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
